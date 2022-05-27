448909f01751908a57a68d7fa13c8901114d8e36
47sanish <78839614+47sanish@users.noreply.github.com>
2022-05-27 11:10:03 +0545

    test cases of sprint 3

---
+++ store/test/test_ulrs.py
@@ -0,0 +1,16 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from store.views import *
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_signup(self):
+        url = reverse('signup')
+        view = resolve(url).func
+        self.assertEquals(view, signup)
+
+    def test_login(self):
+        url = reverse('login')
+        view = resolve(url).func
+        self.assertEquals(view, login)
