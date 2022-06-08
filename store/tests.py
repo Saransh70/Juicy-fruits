71ea4f29860a9318040a66e84bcb722480bdecde
47sanish <78839614+47sanish@users.noreply.github.com>
2022-06-08 22:54:28 +0545

    Added testcases for custom_filter.py

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .templatetags import custom_filter as a
+
 
 # Create your tests here.
+class TestTemplateTags(TestCase):
+    def test_currency_concatinate(self):
+        self.assertEquals(a.currency(1), "₹ 1")
+
+    def test_currency_concatinate_positives(self):
+        self.assertEquals(a.currency(100), "₹ " + '100')
+        self.assertEquals(a.currency(200), "₹ " + '200')
+
+    def test_currency_concatinate_negatives(self):
+        self.assertEquals(a.currency(-100), "₹ " + '100')
+        self.assertEquals(a.currency(-200), "₹ " + '200')
+        self.assertNotEquals(a.currency(-300), "₹ " + '-300')
+
+    def test_currency_concatinate_zeros(self):
+        self.assertEquals(a.currency(0), "₹ " + '0')
+
+
